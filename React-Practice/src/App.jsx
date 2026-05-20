7086bf1d2fb469a204ceb6db0a95c3dadf047d99
Ashish Sharma <ashu11vats@gmail.com>
2026-05-20 19:52:33 +0530

    Updating state using props in React

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from "react";
+
 import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
-import CoreConcept from './components/CoreConcept.jsx';
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton";
+import { EXAMPLES } from "./data.js";
 
 function App() {
+
+  const [selectedTopic, setSelectedTopic] = useState('components');
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(tabContent);
+  }
+
+  console.log('App Component Executing');
+
   return (
     <div>
       <Header />
+
       <main>
         <section id="core-concepts">
           <h2>Time to get started for Core Concepts!</h2>
@@ -16,6 +32,22 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
