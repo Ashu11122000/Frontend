8f84385dab308daaa9a3390669d983aa830f5460
Ashish Sharma <ashu11vats@gmail.com>
2026-05-21 10:02:08 +0530

    Created CoreConcepts.jsx and Examples.jsx components

--- React-Practice/src/App.jsx
+++ React-Practice/src/App.jsx
@@ -1,79 +1,22 @@
-import { useState } from "react";
+// Fragment: A built-in component that allows you to group multiple elements without adding an extra node to the DOM.
+import { useState, Fragment } from "react";
 
 import reactImg from "./assets/react-core-concepts.png";
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton";
-import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-    // console.log(tabContent);
-  }
-
-  console.log("App Component Executing");
 
   return (
-    <div>
+    <Fragment>
       <Header />
 
       <main>
-        <section id="core-concepts">
-          <h2>Time to get started for Core Concepts!</h2>
-          <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-
-          {!selectedTopic ? <p>Please select a topic!</p> : null}
-          {selectedTopic ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          ) : null}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </Fragment>
   );
 }
 
--- React-Practice/src/index.jsx
+++ React-Practice/src/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -5,3 +6,14 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+
+/**
+ * ReactDOM - React library used to connect React with the browser DOM.
+ * createRoot() - This HTML element is where your React app should live.
+ * entrypoint - This is the DOM element where React App will be mounted.
+ * . (dot operator) - Access something inside an object.
+ * render() - Show this UI on the screen.
+ * React - Main Library
+ * createElement - This creates a React Element manually.
+ */
+// ReactDOM.createRoot(entryPoint).render(React.createElement(App));
